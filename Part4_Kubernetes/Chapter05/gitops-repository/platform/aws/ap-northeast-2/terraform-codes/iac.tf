95d2880c4d52cb9fa0acdd3285c687c19d887a4d
hh.yoon <hh.yoon@lge.com>
2023-11-07 08:44:34 +0900

    11/7-2

--- Part4_Kubernetes/Chapter05/gitops-repository/platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ Part4_Kubernetes/Chapter05/gitops-repository/platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,5 +1,5 @@
 module "terraform-codes" {
   #source = "git@github.com:frog97/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
-  source  = "git@github.com:frog97/Part4_Kubernetes/Chapter05/platform-repository/aws/ap-northeast-2/terraform-codes?ref=main"
+  source  = "git@github.com:frog97/FastCampus-k8s.git//Part4_Kubernetes/Chapter05/platform-repository/aws/ap-northeast-2/terraform-codes?ref=main"
  
 }
