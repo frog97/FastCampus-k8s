4c29f1e567c4fdd534751b700c30e0a848437b63
hh.yoon <hh.yoon@lge.com>
2023-11-07 08:30:45 +0900

    11/7

--- Part4_Kubernetes/Chapter05/gitops-repository/platform/aws/ap-northeast-2/terraform-backend/iac.tf
+++ Part4_Kubernetes/Chapter05/gitops-repository/platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -1,3 +1,6 @@
 module "terraform-backend" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+  #source = "git@github.com:frog97/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+  source  = "git@github.com:frog97/FastCampus-k8s.git//Part4_Kubernetes/Chapter05/platform-repository/aws/ap-northeast-2/terraform-backend?ref=main"
+  
+  
 }
